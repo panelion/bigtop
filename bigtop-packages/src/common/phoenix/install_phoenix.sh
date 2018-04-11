f761f791266540f6e9e82d758649fd1b70b4d937
root <root@lean07.nexr.com>
2018-04-11 21:39:21 +0900

    Symbolic link to phoenix-core.jar for Hbase link

--- bigtop-packages/src/common/phoenix/install_phoenix.sh
+++ bigtop-packages/src/common/phoenix/install_phoenix.sh
@@ -137,6 +137,9 @@ ln -s `cd $PREFIX/$LIB_DIR ; ls phoenix*-thin-client.jar` $PREFIX/$LIB_DIR/phoen
 # phoenix-server for placing on the HBase regionserver classpath
 ln -s `cd $PREFIX/$LIB_DIR ; ls phoenix*-server.jar` $PREFIX/$LIB_DIR/phoenix-server.jar
 
+# phoenix-core for placing on the HBase regionserver classpath
+ln -s `cd $PREFIX/$LIB_DIR ; ls phoenix*-core.jar` $PREFIX/$LIB_DIR/phoenix-core.jar
+
 # phoenix-hive
 ln -s `cd $PREFIX/$LIB_DIR ; ls phoenix*-hive.jar` $PREFIX/$LIB_DIR/phoenix-hive.jar
 
